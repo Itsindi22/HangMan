bd4eef31157d4d0ee9742c53a4f1320be86f5d23
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-15 12:59:18 -0500

    Updated JavaScript

--- script.js
+++ script.js
@@ -1,78 +1,41 @@
 var alphabetContainer = document.querySelector('#alphabet-container');
 var wordContainer = document.getElementById('word-container');
+var startGameContainer = document.getElementById('start-game-container');
 var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let randomWord = "";
-var startGameContainer = document.getElementById('start-game-container');
 
 alphabet.split('').forEach(function(letter) {
 
     // Creates a new letter div element
-    var button = document.createElement('div');
-    button.className = 'alphabetLetter';
-    button.textContent = letter;
+    var letterDiv = document.createElement('div');
+    letterDiv.className = 'alphabetLetter';
+    letterDiv.textContent = letter;
 
     // Appends the button to the container
-    alphabetContainer.appendChild(button);
+    alphabetContainer.appendChild(letterDiv);
 })
 
-// randomWord.split("").forEach(function(letter) {
-//     wordContainer.style.display = "flex";
-//     var button = document.createElement('div');
-//     button.className = 'alphabetLetter';
-//     wordContainer.appendChild(button);
-// })
 
-// async function getRandomWord() {
-//     const url = "https://random-word-api.herokuapp.com/word";
+async function getRandomWord() {
+    const url = "https://random-word-api.herokuapp.com/word";
 
-//     try {
-//         const response = await fetch(url);
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         const data = await response.json();
-//         const word = data[0];
-//         randomWord = word;
-//     } catch (error) {
-//         console.log('There was a problem with the fetch operation: ', error);
-//         randomWord = "Error";
-//     }
-// }
-
-function getRandomWord() {
-    return new Promise(async (resolve, reject) => {
-        const url = "https://random-word-api.herokuapp.com/word";
- 
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data = await response.json();
-            const word = data[0];
-            resolve(word);
-        } catch (error) {
-            console.log('There was a problem with the fetch operation: ', error);
-            reject('Error');
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-    });
- }
+        const data = await response.json();
+        const word = data[0];
+        console.log("word: ", word)
+        randomWord = word;
+    } catch (error) {
+        console.log('There was a problem with the fetch operation: ', error);
+        randomWord = "Error";
+    }
+}
 
- 
-// function startGame() {
-//     const startGameContainer = document.getElementById('start-game-container');
-//     const gameDisplay = document.getElementById('gameDisplay');
+getRandomWord()
 
-//     startGameContainer.style.display = "none";
-//     gameDisplay.style.display = "inline";
-    
-//     getRandomWord();
-//  }
-
-getRandomWord().then(word => {
-    randomWord = word;
-    console.log(word);
-})
 async function startGame() {
     const startGameContainer = document.getElementById('start-game-container');
     const gameDisplay = document.getElementById('gameDisplay');
@@ -80,21 +43,17 @@ async function startGame() {
     startGameContainer.style.display = "none";
     gameDisplay.style.display = "inline";
     
-    // await getRandomWord().then(word => {
-    //     randomWord = word;
-    // });
- 
     // Clear existing word container
     while (wordContainer.firstChild) {
         wordContainer.removeChild(wordContainer.firstChild);
     }
  
     // Create new word container based on the random word
-    randomWord.split("").forEach(function(letter) {
+    randomWord.split("").forEach(function() {
         wordContainer.style.display = "flex";
-        var button = document.createElement('div');
-        button.className = 'alphabetLetter';
-        wordContainer.appendChild(button);
+        var letterDiv = document.createElement('div');
+        letterDiv.className = 'alphabetLetter';
+        wordContainer.appendChild(letterDiv);
     })
  }
 
