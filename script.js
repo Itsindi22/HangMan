dbf8de5c0ce5804ebc43e8ce6d736037ccb28670
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-15 12:34:27 -0500

    Updated start design.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var buttonContainer = document.querySelector('#alphabet-container');
+var alphabetContainer = document.querySelector('#alphabet-container');
 var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let randomWord = "";
+var startGameContainer = document.getElementById('start-game-container');
+
 alphabet.split('').forEach(function(letter) {
 
     // Creates a new letter div element
@@ -8,5 +11,44 @@ alphabet.split('').forEach(function(letter) {
     button.textContent = letter;
 
     // Appends the button to the container
-    buttonContainer.appendChild(button);
+    alphabetContainer.appendChild(button);
 })
+
+async function getRandomWord() {
+    const url = "https://random-word-api.herokuapp.com/word";
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const word = data[0];
+        randomWord = word;
+    } catch (error) {
+        console.log('There was a problem with the fetch operation: ', error);
+        randomWord = "Error";
+    }
+}
+
+function startGame() {
+    // getRandomWord();
+    // console.log(randomWord)
+    const startGameContainer = document.getElementById('start-game-container');
+    const alphabetContainer = document.getElementById('alphabet-container');
+    const responseContainer = document.getElementById('response-container');
+
+    startGameContainer.style.display = "none";
+    alphabetContainer.style.display = "flex";
+    responseContainer.style.display = "flex";
+ }
+
+ function resetHeight(element, reduction){
+    // reset the body height to that of the inner browser
+    element.style.height = window.innerHeight-reduction + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", () => resetHeight(startGameContainer, 10));
+
+// called to initially set the height.
+resetHeight(startGameContainer, 10);
