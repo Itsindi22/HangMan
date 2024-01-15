d8e2e8d26a5ad815c0f1f00f005612ad02db6091
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-15 15:09:00 -0500

    User can guess word and can win or lose.

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ var alphabetContainer = document.querySelector('#alphabet-container');
 var wordContainer = document.getElementById('word-container');
 var startGameContainer = document.getElementById('start-game-container');
 var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var alphabetList = [];
 let randomWord = "";
+var randomWordDict = {};
+let randomWordLength = 0;
+let gameEndBoolean = false;
+let playerTries = 6;
 
 alphabet.split('').forEach(function(letter) {
 
     // Creates a new letter div element
-    var letterDiv = document.createElement('button');
-    letterDiv.className = 'alphabetLetter';
-    letterDiv.id = "alphabetBtn";
-    letterDiv.textContent = letter;
+    var letterButton = document.createElement('button');
+    letterButton.className = 'alphabetLetter';
+    letterButton.id = "alphabetBtn";
+    letterButton.textContent = letter;
 
     // Appends the button to the container
-    alphabetContainer.appendChild(letterDiv);
+    alphabetContainer.appendChild(letterButton);
+    alphabetList.push(letterButton)
 })
 
 
@@ -29,6 +35,23 @@ async function getRandomWord() {
         const word = data[0];
         console.log("word: ", word)
         randomWord = word;
+        randomWordLength = randomWord.length;
+
+        // Clear existing word container
+        while (wordContainer.firstChild) {
+            wordContainer.removeChild(wordContainer.firstChild);
+        }
+    
+        // Create new word container based on the random word
+        randomWord.split("").forEach(function(letter, index) {
+            wordContainer.style.display = "flex";
+            var letterDiv = document.createElement('div');
+            letterDiv.className = 'alphabetLetter';
+            letterDiv.id = "alphabetBtn" + index;
+            wordContainer.appendChild(letterDiv);
+            
+            randomWordDict[index] = [letterDiv, letter];
+        })
     } catch (error) {
         console.log('There was a problem with the fetch operation: ', error);
         randomWord = "Error";
@@ -43,19 +66,6 @@ async function startGame() {
  
     startGameContainer.style.display = "none";
     gameDisplay.style.display = "inline";
-    
-    // Clear existing word container
-    while (wordContainer.firstChild) {
-        wordContainer.removeChild(wordContainer.firstChild);
-    }
- 
-    // Create new word container based on the random word
-    randomWord.split("").forEach(function() {
-        wordContainer.style.display = "flex";
-        var letterDiv = document.createElement('div');
-        letterDiv.className = 'alphabetLetter';
-        wordContainer.appendChild(letterDiv);
-    })
  }
 
  
@@ -68,3 +78,61 @@ window.addEventListener("resize", () => resetHeight(startGameContainer, 10));
 
 // called to initially set the height.
 resetHeight(startGameContainer, 10);
+
+alphabetList.forEach(function(letterButton) {
+    letterButton.addEventListener("click", function() {
+        if (!gameEndBoolean) {
+            var letter = this.textContent.toLowerCase();
+            var letterInWord = false;
+            console.log("Letter: " + letter)
+
+            Object.keys(randomWordDict).forEach((key) => {
+                var data = randomWordDict[key];
+                var letterDiv = data[0];
+                var wordLetter = data[1];
+                
+                if (letter === wordLetter.toLowerCase()) {
+                    letterInWord = true;
+                    letterDiv.textContent = wordLetter;
+
+                    if (letterButton.style.backgroundColor !== "green") {
+                        randomWordLength -= 1;
+                    }
+                }
+            })
+
+            if (letterInWord) {
+                letterButton.style.backgroundColor = "green";
+            } else {
+                letterButton.style.backgroundColor = "var(--title)";
+                playerTries -= 1;
+
+                if (playerTries <= 0) {
+                    gameEnd("lose");
+                }
+            }
+
+            if (randomWordLength <= 0) {
+                gameEnd("win");
+            }
+        }
+    })
+})
+
+function gameEnd(state) {
+    gameEndBoolean = true;
+    var backgroundColor = "var(--text)";
+
+    if (state === "win") {
+        backgroundColor = "green";
+    } else {
+        backgroundColor = "var(--title)"
+    }
+
+    Object.keys(randomWordDict).forEach((key) => {
+        var data = randomWordDict[key];
+        var letterDiv = data[0];
+
+        letterDiv.style.backgroundColor = backgroundColor;
+    })
+}
