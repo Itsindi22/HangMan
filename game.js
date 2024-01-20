913a1c53eceb0f44a975f654b737475ea0f9636f
MochaWaffle <vsaini23@medford.k12.ma.us>
2024-01-20 16:43:49 -0500

    Added key presses.
    Player's can now press keys to guess the word (alphabetical letters only).

--- game.js
+++ game.js
@@ -1,39 +1,43 @@
-var alphabetContainer = document.querySelector('#alphabet-container');
-var wordContainer = document.getElementById('word-container');
-var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var alphabetContainer = document.querySelector("#alphabet-container");
+var wordContainer = document.getElementById("word-container");
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var alphabetList = [];
 let randomWord = "";
 var randomWordDict = {};
 let randomWordLength = 0;
 let gameEndBoolean = false;
 let playerTries = 6;
-var head = document.getElementById('head');
-var body = document.getElementById('body');
-var leftArm = document.getElementById('left-arm');
-var rightArm = document.getElementById('right-arm')
-var leftLeg = document.getElementById('left-leg')
-var rightLeg = document.getElementById('right-leg')
-var hangManPieces = [head, body, leftArm, rightArm, leftLeg, rightLeg]
+var head = document.getElementById("head");
+var body = document.getElementById("body");
+var leftArm = document.getElementById("left-arm");
+var rightArm = document.getElementById("right-arm");
+var leftLeg = document.getElementById("left-leg");
+var rightLeg = document.getElementById("right-leg");
+var hangManPieces = [head, body, leftArm, rightArm, leftLeg, rightLeg];
 var centerDiv = document.getElementById("center-div");
 var endMessage = document.getElementById("end-message");
+var resetButton = document.getElementById("resetButton");
 
-document.getElementById("resetButton").onclick = function() {
-    location.href= "game.html"
-    // getRandomWord();
-    // drawAlphabets();
+resetButton.onclick = function () {
+    resetGame()
 };
 
+function resetGame() {
+    location.href = "game.html";
+    // getRandomWord();
+    // drawAlphabets();
+}
 function drawAlphabets() {
-    alphabet.split('').forEach(function(letter) {
+    alphabet.split("").forEach(function (letter) {
         // Creates a new letter div element
-        var letterButton = document.createElement('button');
-        letterButton.className = 'alphabetLetter';
+        var letterButton = document.createElement("button");
+        letterButton.className = "alphabetLetter";
         letterButton.id = "alphabetBtn";
         letterButton.textContent = letter;
-    
+
         // Appends the button to the container
         alphabetContainer.appendChild(letterButton);
-        alphabetList.push(letterButton)
+        alphabetList.push(letterButton);
     });
 }
 
@@ -55,37 +59,37 @@ async function getRandomWord() {
         while (wordContainer.firstChild) {
             wordContainer.removeChild(wordContainer.firstChild);
         }
-    
+
         // Create new word container based on the random word
-        randomWord.split("").forEach(function(letter, index) {
+        randomWord.split("").forEach(function (letter, index) {
             wordContainer.style.display = "flex";
-            var letterDiv = document.createElement('div');
-            letterDiv.className = 'alphabetLetter';
+            var letterDiv = document.createElement("div");
+            letterDiv.className = "alphabetLetter";
             letterDiv.id = "alphabetBtn" + index;
             wordContainer.appendChild(letterDiv);
-            
+
             randomWordDict[index] = [letterDiv, letter];
-        })
+        });
     } catch (error) {
-        console.log('There was a problem with the fetch operation: ', error);
+        console.log("There was a problem with the fetch operation: ", error);
         randomWord = "Error";
     }
 }
 
 getRandomWord();
 drawAlphabets();
-alphabetList.forEach(function(letterButton) {
-    letterButton.addEventListener("click", function() {
+alphabetList.forEach(function (letterButton) {
+    letterButton.addEventListener("click", function () {
         if (!gameEndBoolean) {
             var letter = this.textContent.toLowerCase();
             var letterInWord = false;
-            console.log("Letter: " + letter)
+            console.log("Letter: " + letter);
 
             Object.keys(randomWordDict).forEach((key) => {
                 var data = randomWordDict[key];
                 var letterDiv = data[0];
                 var wordLetter = data[1];
-                
+
                 if (letter === wordLetter.toLowerCase()) {
                     letterInWord = true;
                     letterDiv.textContent = wordLetter;
@@ -94,21 +98,21 @@ alphabetList.forEach(function(letterButton) {
                         randomWordLength -= 1;
                     }
                 }
-            })
+            });
 
             if (letterInWord) {
                 letterButton.style.backgroundColor = "green";
             } else {
                 if (letterButton.style.backgroundColor != "var(--title)") {
                     playerTries -= 1;
-                    endMessage.innerHTML = "Attempts Left: " + playerTries; 
+                    endMessage.innerHTML = "Attempts Left: " + playerTries;
                     removeCenterId();
                     var piece = hangManPieces.shift();
-                    piece.style.display = 'block';
+                    piece.style.display = "block";
                 }
 
                 letterButton.style.backgroundColor = "var(--title)";
-                
+
                 if (playerTries <= 0) {
                     gameEnd("lose");
                 }
@@ -118,39 +122,39 @@ alphabetList.forEach(function(letterButton) {
                 gameEnd("win");
             }
         }
-    })
-})
+    });
+});
 
 function gameEnd(state) {
     gameEndBoolean = true;
     var backgroundColor = "var(--text)";
-    var bloodDisplay = document.getElementById('blood-container');
+    var bloodDisplay = document.getElementById("blood-container");
 
     if (state === "win") {
         backgroundColor = "green";
-        var attemptsLeft = ""
+        var attemptsLeft = "";
 
         if (playerTries > 1) {
-            attemptsLeft = playerTries + " attempts"
+            attemptsLeft = playerTries + " attempts";
         } else {
             attemptsLeft = playerTries + " attempt";
         }
 
         endMessage.innerHTML = `Congratulations!! You won! You had ${attemptsLeft} left!`;
     } else {
-        backgroundColor = "var(--title)"
-        bloodDisplay.style.display = 'flex'
+        backgroundColor = "var(--title)";
+        bloodDisplay.style.display = "flex";
         endMessage.innerHTML = "Game Over: You Lost. Try again?";
     }
 
     Object.keys(randomWordDict).forEach((key) => {
         var data = randomWordDict[key];
         var letterDiv = data[0];
-        var wordLetter = data[1]
+        var wordLetter = data[1];
 
         letterDiv.style.backgroundColor = backgroundColor;
         letterDiv.textContent = wordLetter;
-    })
+    });
 }
 
 function resetHeight(element, reduction) {
@@ -160,7 +164,7 @@ function resetHeight(element, reduction) {
 }
 
 function removeCenterId() {
-    centerDiv.style.display = "block"
+    centerDiv.style.display = "block";
     centerDiv.style.height = "auto";
     centerDiv.style.border = "none";
 }
@@ -168,3 +172,56 @@ function removeCenterId() {
 resetHeight(centerDiv, 120);
 
 window.addEventListener("resize", () => resetHeight(centerDiv, 120));
+
+window.addEventListener("keydown", function (event) {
+    letterKeyPress(event.key);
+});
+
+function letterKeyPress(letterKey) {
+    alphabetList.forEach(function (letterButton) {
+        if (!gameEndBoolean) {
+            var letter = letterButton.textContent.toLowerCase();
+            if (letter === letterKey) {
+                var letterInWord = false;
+                console.log("Letter: " + letter);
+
+                Object.keys(randomWordDict).forEach((key) => {
+                    var data = randomWordDict[key];
+                    var letterDiv = data[0];
+                    var wordLetter = data[1];
+
+                    if (letter === wordLetter.toLowerCase()) {
+                        letterInWord = true;
+                        letterDiv.textContent = wordLetter;
+
+                        if (letterButton.style.backgroundColor !== "green") {
+                            randomWordLength -= 1;
+                        }
+                    }
+                });
+
+                if (letterInWord) {
+                    letterButton.style.backgroundColor = "green";
+                } else {
+                    if (letterButton.style.backgroundColor != "var(--title)") {
+                        playerTries -= 1;
+                        endMessage.innerHTML = "Attempts Left: " + playerTries;
+                        removeCenterId();
+                        var piece = hangManPieces.shift();
+                        piece.style.display = "block";
+                    }
+
+                    letterButton.style.backgroundColor = "var(--title)";
+
+                    if (playerTries <= 0) {
+                        gameEnd("lose");
+                    }
+                }
+
+                if (randomWordLength <= 0) {
+                    gameEnd("win");
+                }
+            }
+        }
+    });
+}
